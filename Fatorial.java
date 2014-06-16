d9c98f4c064ac61c8a1459fe26bc9f713ca9a6e8
Thiago Aparecido Aoki <thiago@publisoft.com.br>
2014-06-16 12:47:44 -0300

    Comentario em Fatorial.java atualizados

--- Fatorial.java
+++ Fatorial.java
@@ -1,5 +1,5 @@
 public class Fatorial{
-  // Versao iterativa do calculo de fatorial
+  // Versao iterativa do calculo de fatorial, incluindo o teste para n=0
   public int calcularFatorial(int n) throws IllegalArgumentException {
     if (n<0) 
       throw new IllegalArgumentException("não existem fatorial para numero negativos");
