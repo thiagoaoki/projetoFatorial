106a9f5e4ef77815122ea8a41663f3170c20f007
Thiago Aparecido Aoki <thiago@publisoft.com.br>
2014-06-16 12:58:15 -0300

    Calculo do fatorial de 4 em vez de 0

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ class TesteFatorial{
   // Comentario inserido em TesteFatorial.java
   public static void main(String[] args) {
     Fatorial fat = new Fatorial();
-    int resultado = fat.calcularFatorial(0);
+    int resultado = fat.calcularFatorial(4);
     System.out.println(resultado);
   }
 }
